c7e7550c41be869313cd496d7af0b172841961f1
Luiz Moraes <luizgustavofm09@gmail.com>
2026-05-26 16:17:51 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,16 @@
+n1 = float(input("Nota 1: "))
+n2 = float(input("Nota 2: "))
+n3 = float(input("Nota 3: "))
+
+media = (n1 + n2 + n3) / 3
+
+print("Média:", media)
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
+
+
